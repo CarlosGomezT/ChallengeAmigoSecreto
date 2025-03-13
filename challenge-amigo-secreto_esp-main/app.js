6c379b7813d2ba69603020a0531c3018ba59c0ee
Carlos Gomez <carlosyoshy10@gmail.com>
2025-03-13 02:42:02 -0600

    Challenge completado y extra
    Se terminaron los puntos basicos del challenge y se agregaron funcionalidades extras para enumerar a la lista de amigos, asi como reiniciar las listas desde 0

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombresSorteados = [];
+
+limpiarLista();
+
+function recibirValor() {
+    return document.getElementById(`amigo`).value;
+}
+
+function agregarAmigo() {
+    var valorEnCaja = recibirValor();
+    if(valorEnCaja == ``){
+        alert(`Porfvor ingrese un nombre`);
+    }
+    else {
+        listaNombresSorteados.push(valorEnCaja);
+        document.getElementById(`amigo`).value=``;
+        document.getElementById(`amigo`).focus();
+    }
+    recorrerLista();
+}
+
+function recorrerLista(){
+    let listaAmigos = document.getElementById(`listaAmigos`)
+    listaAmigos.innerHTML = ``;
+
+    for(i=0; i<listaNombresSorteados.length; i++ )
+    {
+        let elementoHTML = document.createElement(`li`);
+        elementoHTML.textContent = `Num ${i+1} .- ${listaNombresSorteados[i]}`;
+        listaAmigos.appendChild(elementoHTML);
+    }
+}
+function sortearAmigo() {
+    if (listaNombresSorteados.length==0)
+    {
+        alert(`No hay amigos en la lista`);
+        return;
+    }
+    let amigoSecreto = listaNombresSorteados[Math.floor(Math.random()* listaNombresSorteados.length)];
+    let resultado = document.getElementById(`resultado`);
+    resultado.innerHTML = `Tu amigo secreto es: ${amigoSecreto}`;
+}
+function limpiarLista() {
+    listaNombresSorteados = [];
+    recorrerLista();
+    document.getElementById(`resultado`).innerHTML = ``;
+}
